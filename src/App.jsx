ac187ac220626fa8a342d5c50ec11ada3bda815f
Furkan Ünsalan <furkanunsalan@gmail.com>
2024-01-29 18:03:57 +0300

    edited ui fixed mobile added somethings

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Calculator from "./components/Calculator.jsx";
 import Navbar from "./components/Navbar.jsx"
 import Footer from "./components/Footer.jsx";
 import Entry from "./components/Entry.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
     return <>
+        <SpeedInsights/>
         <Navbar/>
         <Entry/>
         <Calculator/>
